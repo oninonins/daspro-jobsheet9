0a5a5ae2570f7e519b77ea852d167839b42e23cf
oninonins <varelinobubadibako11@gmail.com>
2024-10-31 13:41:24 +0700

    revisi1

--- ArrayNilai05.java
+++ ArrayNilai05.java
@@ -6,13 +6,18 @@ public class ArrayNilai05 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukan nilai akhir ke-" +i+" : ");
             nilaiAkhir[i] = input.nextInt();
 
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" +i+ " adalah " +nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" +i+ " lulus!");
+                
+            }else {
+                System.out.println("Mahasiswa ke-" +i+ " tidak lulus!");
+            }
         }
     }
 }
