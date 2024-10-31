77f710b79d667485595a03156a0f5361c3f1137d
oninonins <varelinobubadibako11@gmail.com>
2024-10-31 13:14:04 +0700

    new

---
+++ ArrayNilai05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayNilai05 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukan nilai akhir ke-" +i+" : ");
+            nilaiAkhir[i] = input.nextInt();
+
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" +i+ " adalah " +nilaiAkhir[i]);
+        }
+    }
+}
